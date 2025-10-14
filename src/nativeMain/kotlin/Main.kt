30df04e5b5dba8e651d11ef85369c12d6124bc15
ternbusty <a.iwasaki0128@gmail.com>
2025-10-14 01:48:18 +0000

    add unshare example

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,20 +1,32 @@
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.cValuesOf
+import kotlinx.cinterop.memScoped
+import platform.linux.__NR_unshare
+import platform.posix.*
 
-@Serializable
-private data class Message(
-    val topic: String,
-    val content: String,
-)
+private const val CLONE_NEWPID: Int = 0x20000000
 
-private val PrettyPrintJson = Json {
-    prettyPrint = true
-}
-
-fun main() {
-    val message = Message(
-        topic = "Kotlin/Native",
-        content = "Hello!"
-    )
-    println(PrettyPrintJson.encodeToString(message))
+@OptIn(ExperimentalForeignApi::class)
+fun main() = memScoped {
+    fprintf(stderr, "parent getpid=%d getppid=%d\n", getpid(), getppid())
+    val rc = syscall(__NR_unshare.toLong(), CLONE_NEWPID)
+    if (rc != 0L) {
+        perror("unshare")
+        exit(1)
+    }
+    val pid = fork()
+    if (pid == -1) {
+        perror("fork")
+        exit(1)
+    }
+    if (pid == 0) {
+        fprintf(stderr, "child getpid=%d getppid=%d\n", getpid(), getppid())
+        _exit(0)
+    } else {
+        var status = 0
+        if (waitpid(pid, cValuesOf(status), 0) == -1) {
+            perror("waitpid")
+            exit(1)
+        }
+    }
 }
