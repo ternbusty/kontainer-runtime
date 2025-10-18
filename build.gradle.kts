3190c9b23aa985c4fafcb702b90c65f8e27a096c
ternbusty <a.iwasaki0128@gmail.com>
2025-10-18 02:31:09 +0000

    introduce kotest

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
 plugins {
-    alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.kotlinxSerialization)
+    kotlin("multiplatform") version "2.2.20"
+    kotlin("plugin.serialization") version "2.2.20"
+    id("io.kotest") version "6.0.4"
+    id("com.google.devtools.ksp") version "2.2.20-2.0.4"
 }
 
 group = "me.user"
 version = "1.0-SNAPSHOT"
+val kotestVersion = "6.0.4"
 
 repositories {
     mavenCentral()
@@ -40,6 +43,10 @@ kotlin {
     sourceSets {
         nativeMain.dependencies {
             implementation(libs.kotlinxSerializationJson)
+        }
+        nativeTest.dependencies {
+            implementation("io.kotest:kotest-assertions-core:${kotestVersion}")
+            implementation("io.kotest:kotest-framework-engine:${kotestVersion}")
         }
     }
 }
